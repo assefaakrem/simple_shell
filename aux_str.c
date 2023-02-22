47f261d3edd800fb16c1d30135b4fd4b359e51b2
assefaakrem <assefaakrem@gmail.com>
2023-02-22 03:40:04 -0800

    correct the error

--- aux_str.c
+++ aux_str.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two strings
+ * _strcat - a function which concatenate two strings
  * @dest: char pointer the dest of the copied str
  * @src: const char pointer the source of str
  * Return: the dest
