d1f2569bf08b53440cc1540fef86c04903cfad5a
Anonymous9995 <118614958+Anonymous9995@users.noreply.github.com>
2023-02-24 12:51:40 -0500

    Update get_error.c

--- get_error.c
+++ get_error.c
@@ -2,7 +2,7 @@
 
 /**
  * get_error - calls the error according the builtin, syntax or permission
- * @datash: data structure that contains arguments
+ * @datash: data structure that contains argument
  * @eval: error value
  * Return: error
  */
