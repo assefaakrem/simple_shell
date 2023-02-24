ac1a9089d048e55683d17890636cc9ae44fc4485
Anonymous9995 <118614958+Anonymous9995@users.noreply.github.com>
2023-02-24 12:46:05 -0500

    swap

--- split.c
+++ split.c
@@ -4,7 +4,7 @@
  * swap_char - swaps | and & for non-printed chars
  *
  * @input: input string
- * @bool: type of swap
+ * @bool: type of  swap
  * Return: swapped string
  */
 char *swap_char(char *input, int bool)
